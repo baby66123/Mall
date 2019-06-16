c7659c527738cd75fb35369aa202f54375c7ff74
RanDY <baby6675525@gmail.com>
2019-06-16 23:13:11 +0800

    SignUp,Login

--- app/src/main/java/com/randy/mall/MainActivity.kt
+++ app/src/main/java/com/randy/mall/MainActivity.kt
@@ -1,25 +1,68 @@
 package com.randy.mall
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),FirebaseAuth.AuthStateListener {
+
+
+    private val RC_SIGNIN: Int=100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        verify_email.setOnClickListener {
+            FirebaseAuth.getInstance().currentUser?.sendEmailVerification()
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        Snackbar.make(it,"Verify Email Sent",Snackbar.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+
+    override fun onAuthStateChanged(auth: FirebaseAuth) {
+        val user=auth.currentUser
+        if(user!=null){
+            user_info.setText("Email: ${user.email} / ${user.isEmailVerified}")
+            verify_email.visibility=if (user.isEmailVerified)View.GONE else View.VISIBLE
+        }else{
+            user_info.setText("Not Login")
+            verify_email.visibility=View.GONE
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FirebaseAuth.getInstance().removeAuthStateListener(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,6 +77,33 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
+            R.id.action_signin -> {
+                val whiteList= listOf<String>("tw","hk","cn","au")
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                            AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.GoogleBuilder().build(),
+                            AuthUI.IdpConfig.FacebookBuilder().build(),
+                            AuthUI.IdpConfig.PhoneBuilder()
+                                .setWhitelistedCountries(whiteList)
+                                .setDefaultCountryIso("tw")
+                                .build()
+                        ))
+                        .setIsSmartLockEnabled(false)
+                        .setLogo(R.drawable.mall)
+                        .setTheme(R.style.SignUp)
+                        .build(),RC_SIGNIN)
+//                startActivityForResult(
+//                    Intent(this,SignInActivity::class.java),
+//                    RC_SIGNIN)
+                true
+            }
+            R.id.action_signout ->{
+                FirebaseAuth.getInstance().signOut()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
